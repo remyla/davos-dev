f666c046267f2c648386929873ccf66a5d9d444e
sebcourtois <neitsabes974@gmail.com>
2015-08-19 12:55:11 +0200

    minor update on AssetBrowserWindow

--- davos/gui/assetbrowserwindow.py
+++ davos/gui/assetbrowserwindow.py
@@ -19,12 +19,14 @@ class AssetBrowserWindow(QtGui.QMainWindow, Ui_AssetBrowserWin):
 
     classBrowserWidget = AssetBrowserWidget
 
-    def __init__(self, sWindowName, windowTitle="", parent=None):
+    def __init__(self, windowName="", windowTitle="", parent=None):
         super(AssetBrowserWindow, self).__init__(parent)
 
         self.setupUi(self)
-        self.setObjectName(sWindowName)
-        self.setWindowTitle(windowTitle if windowTitle else labelify(sWindowName))
+        if windowName:
+            self.setObjectName(windowName)
+        if windowTitle:
+            self.setWindowTitle(windowTitle)
 
         self.browserWidget = self.__class__.classBrowserWidget(self)
         self.setCentralWidget(self.browserWidget)
